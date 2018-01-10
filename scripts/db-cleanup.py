3ec1c93866c0dc6a0053fd730e4312eb4959b4d1
Thomas Graichen <thomas.graichen@sap.com>
2018-01-10 09:54:38 +0100

    add comments on how it works

--- scripts/db-cleanup.py
+++ scripts/db-cleanup.py
@@ -12,16 +12,24 @@ from openstack import connection
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
 
+# list of servers or volumes we have seen or plan to delete
 servers_to_be_deleted = dict()
 servers_seen = dict()
 volumes_to_be_deleted = dict()
 volumes_seen = dict()
 
+# cmdline handling
 @click.command()
+# every how many minutes the check should be preformed
 @click.option('--interval', prompt='Interval in minutes')
+# how often a vm should be continously a canditate for some action (delete etc.) before
+# we actually do it - the idea behind is that we want to avoid actions due to short
+# temporary technical problems of any kind
 @click.option('--iterations', prompt='Iterations')
+# work on nova db (vms) or cinder db (volumes)?
 @click.option('--nova', is_flag=True)
 @click.option('--cinder', is_flag=True)
+# dry run mode - only say what we would do without actually doing it
 @click.option('--dry-run', is_flag=True)
 def run_me(interval, iterations, nova, cinder, dry_run):
     if nova or cinder:
@@ -35,11 +43,13 @@ def init_seen_dict(seen_dict):
     for i in seen_dict:
         seen_dict[i] = 0
 
+# reset dict of all vms or volumes we plan to delete from the db
 def reset_to_be_dict(to_be_dict, seen_dict):
     for i in seen_dict:
         if seen_dict[i] == 0:
             to_be_dict[i] = 0
 
+# here we decide to wait longer before doings the delete from the db or finally doing it
 def now_or_later(id, to_be_dict, seen_dict, what_to_do, iterations):
     default = 0
     seen_dict[id] = 1
@@ -50,7 +60,9 @@ def now_or_later(id, to_be_dict, seen_dict, what_to_do, iterations):
             log.info("- considering later %s %s (%i/%i)", what_to_do, id, to_be_dict.get(id, default) + 1, int(iterations))
         to_be_dict[id] = to_be_dict.get(id, default) + 1
 
+# main cleanup function
 def os_cleanup_items(interval, iterations, nova, cinder, dry_run):
+    # openstack connection
     conn = connection.Connection(auth_url=os.getenv('OS_AUTH_URL'),
                                  project_name=os.getenv('OS_PROJECT_NAME'),
                                  project_domain_name=os.getenv('OS_PROJECT_DOMAIN_NAME'),
@@ -62,41 +74,51 @@ def os_cleanup_items(interval, iterations, nova, cinder, dry_run):
     servers = dict()
     volumes = dict()
 
+    # get all openstack projects
     for project in conn.identity.projects(details=False, all_tenants=1):
         projects[project.id] = project.name
 
+    # get all instances from nova
     if nova:
         for server in conn.compute.servers(details=True, all_tenants=1):
             servers[server.id] = server.project_id
         init_seen_dict(servers_seen)
         for aserver, aprojectid in six.iteritems(servers):
+            # instance has an existing project id - we keep it
             if projects.get(aprojectid):
                 log.debug("server %s has a valid project id: %", str(aserver), str(aprojectid))
                 pass
+            # instance has no existing project id - we plan to delete it
             else:
                 if not dry_run:
                     log.info("- should not get here")
                 else:
                     log.debug("server %s has no valid project id!", str(aserver))
                     now_or_later(aserver, servers_to_be_deleted, servers_seen, "delete of server", iterations)
+        # reset the dict of instances we plan to do delete from the db for all machines we did not see or which disappeared
         reset_to_be_dict(servers_to_be_deleted, servers_seen)
 
+    # get all volumes from cinder
     if cinder:
         for volume in conn.block_store.volumes(details=True, all_tenants=1):
             volumes[volume.id] = volume.project_id
         init_seen_dict(volumes_seen)
         for avolume, aprojectid in six.iteritems(volumes):
+            # volume has an existing project id - we keep it
             if projects.get(aprojectid):
                 log.debug("volume %s has a valid project id: %", str(avolume), str(aprojectid))
                 pass
+            # volume has no existing project id - we plan to delete it
             else:
                 if not dry_run:
                     log.info("- should not get here")
                 else:
                     log.debug("volume %s has no valid project id!", str(avolume))
                     now_or_later(avolume, volumes_to_be_deleted, volumes_seen, "delete of volume", iterations)
+        # reset the dict of instances we plan to do delete from the db for all machines we did not see or which disappeared
         reset_to_be_dict(volumes_to_be_deleted, volumes_seen)
 
+    # wait the interval time
     time.sleep(60 * int(interval))
 
 if __name__ == '__main__':
--- scripts/vcenter-cleanup.py
+++ scripts/vcenter-cleanup.py
@@ -25,6 +25,7 @@ vms_seen = dict()
 files_to_be_deleted = dict()
 files_seen = dict()
 
+# find files with a uuid name pattern
 def _uuids(task):
     folder_path = task.info.result.folderPath
     for f in task.info.result.file:
@@ -32,26 +33,40 @@ def _uuids(task):
         if match:
             yield match.group(0), {'datastore': folder_path, 'folder': f.path}
 
+# cmdline handling
 @click.command()
+# vcenter host, user and password
 @click.option('--host', help='Host to connect to.')
 @click.option('--username', prompt='Your name')
 @click.option('--password', prompt='The password')
+# every how many minutes the check should be preformed
 @click.option('--interval', prompt='Interval in minutes')
+# how often a vm should be continously a canditate for some action (delete etc.) before
+# we actually do it - the idea behind is that we want to avoid actions due to short
+# temporary technical problems of any kind ... another idea is to do the actions step
+# by step (i.e. power-off - iterations - unlink - iterations - delete file path), so that
+# we have a chance to still roll back in case we notice problems due to some wrong
+# action done
 @click.option('--iterations', prompt='Iterations')
+# dry run mode - only say what we would do without actually doing it
 @click.option('--dry-run', is_flag=True)
 def run_me(host, username, password, interval, iterations, dry_run):
     while True:
         cleanup_items(host, username, password, interval, iterations, dry_run)
 
+# init dict of all vms or files we have seen already
 def init_seen_dict(seen_dict):
     for i in seen_dict:
         seen_dict[i] = 0
 
+# reset dict of all vms or files we plan to do something with (delete etc.)
 def reset_to_be_dict(to_be_dict, seen_dict):
     for i in seen_dict:
+        # if a machine we planned to delete no longer appears as canditate for delettion, remove it from the list
         if seen_dict[i] == 0:
             to_be_dict[i] = 0
 
+# here we decide to wait longer before doings something (delete etc.) or finally doing it
 def now_or_later(id, to_be_dict, seen_dict, what_to_do, iterations):
     default = 0
     seen_dict[id] = 1
@@ -63,7 +78,9 @@ def now_or_later(id, to_be_dict, seen_dict, what_to_do, iterations):
                      int(iterations))
         to_be_dict[id] = to_be_dict.get(id, default) + 1
 
+# main cleanup function
 def cleanup_items(host, username, password, interval, iterations, dry_run):
+    # openstack connection
     conn = connection.Connection(auth_url=os.getenv('OS_AUTH_URL'),
                                  project_name=os.getenv('OS_PROJECT_NAME'),
                                  project_domain_name=os.getenv('OS_PROJECT_DOMAIN_NAME'),
@@ -73,6 +90,7 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
 
     known = dict()
 
+    # get all servers, volumes, snapshots and images from openstack to compare the resources we find on the vcenter against
     for server in conn.compute.servers(details=False, all_tenants=1):
         known[server.id] = server
 
@@ -85,6 +103,7 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
     for image in conn.image.images(details=False, all_tenants=1):
         known[image.id] = image
 
+    # vcenter connection
     if hasattr(ssl, '_create_unverified_context'):
         context = ssl._create_unverified_context()
 
@@ -102,6 +121,7 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
     content = service_instance.content
     dc = content.rootFolder.childEntity[0]
     missing = dict()
+    # iterate through all datastores in the vcenter
     for ds in dc.datastore:
         log.info("- datacenter / datastore: %s / %s", dc.name, ds.name)
 
@@ -110,6 +130,7 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
 
         try:
             WaitForTask(task, si=service_instance)
+            # find all the entities we have on the vcenter which have no relation to openstack anymore and write them to a dict
             for uuid, location in _uuids(task):
                 if uuid not in known:
                     missing[uuid] = location
@@ -121,18 +142,23 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
     init_seen_dict(vms_seen)
     init_seen_dict(files_seen)
 
+    # iterate over all entities we have on the vcenter which have no relation to openstack anymore
     for item, location in six.iteritems(missing):
+        # find the vm correspoding to the file path
         path = "{datastore} {folder}".format(**location)
         vmx_path = "{datastore} {folder}/{folder}.vmx".format(**location)
         vm = content.searchIndex.FindByDatastorePath(path=vmx_path, datacenter=dc)
+        # there is a vm for that file path
         if vm:
             power_state = vm.runtime.powerState
+            # we store the openstack project id in the annotations of the vm
             annotation = vm.config.annotation or ''
             items = dict([line.split(':', 1) for line in annotation.splitlines()])
             if 'projectid' in items:
                 log.debug(
                     "{folder}: {power_state} {projectid}".format(power_state=power_state, projectid=items['projectid'],
                                                                  **location))
+                # if still powered on the panned action is to power off
                 if power_state == 'poweredOn':
                     if not dry_run:
                         log.info("- should not get here")
@@ -143,8 +169,10 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
                     log.info("- should not get here")
                     # better unlink the vm only, i.e. leave the files on disk
                     # tasks.append(vm.Destroy_Task())
+                # if already powered off the planned action is to unlink the vm
                 else:
                     now_or_later(vm.config.uuid, vms_to_be_unregistered, vms_seen, "unregister of vm", iterations)
+        # there is no vm anymore for the file path - planned action is to delete the file
         else:
             if not dry_run:
                 log.info("- should not get here")
@@ -154,9 +182,12 @@ def cleanup_items(host, username, password, interval, iterations, dry_run):
                              iterations)
         if len(tasks) % 8 == 0:
             WaitForTasks(tasks[-8:], si=service_instance)
+    # reset the dict of vms or files we plan to do something with for all machines we did not see or which disappeared
     reset_to_be_dict(vms_to_be_poweredoff, vms_seen)
     reset_to_be_dict(vms_to_be_unregistered, vms_seen)
     reset_to_be_dict(files_to_be_deleted, files_seen)
+
+    # wait the interval time
     time.sleep(60 * int(interval))
 
 if __name__ == '__main__':
