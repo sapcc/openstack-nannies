b64c721557ea124dfb2505bad5a357187766455a
Thomas Graichen <thomas.graichen@sap.com>
2018-03-01 15:25:51 +0100

    rewrite as class and some other improvements

--- scripts/db-cleanup.py
+++ scripts/db-cleanup.py
@@ -47,108 +47,127 @@ volumes_seen = dict()
 @click.option('--cinder', is_flag=True)
 # dry run mode - only say what we would do without actually doing it
 @click.option('--dry-run', is_flag=True)
+class Cleanup:
+    def __init__(self, interval, iterations, nova, cinder, dry_run):
+        self.interval = interval
+        self.iterations = iterations
+        self.nova = nova
+        self.cinder = cinder
+        self.dry_run = dry_run
 
-def run_me(interval, iterations, nova, cinder, dry_run):
+        # a dict of all projects we have in openstack
+        self.projects = dict()
 
-    if nova or cinder:
-        while True:
-            os_cleanup_items(interval, iterations, nova, cinder, dry_run)
-    else:
-        log.info("either the --nova or the --cinder flag should be given - giving up!")
-        sys.exit(0)
+        # dicts for the ids we have seen and the ones we want to do something with
+        self.seen_dict = dict()
+        self.to_be_dict = dict()
 
-def init_seen_dict(seen_dict):
-    for i in seen_dict:
-        seen_dict[i] = 0
+        self.run_me()
 
-# reset dict of all vms or volumes we plan to delete from the db
-def reset_to_be_dict(to_be_dict, seen_dict):
-    for i in seen_dict:
-        if seen_dict[i] == 0:
-            to_be_dict[i] = 0
-
-# here we decide to wait longer before doings the delete from the db or finally doing it
-def now_or_later(id, to_be_dict, seen_dict, what_to_do, iterations, dry_run, conn):
-    default = 0
-    seen_dict[id] = 1
-    if to_be_dict.get(id, default) <= int(iterations):
-        if to_be_dict.get(id, default) == int(iterations):
-            if dry_run:
-                log.info("- dry-run: %s %s", what_to_do, id)
-            else:
-                if what_to_do == "delete of server":
-                    log.info("- action: %s %s", what_to_do, id)
-                    try:
-                        conn.compute.delete_server(id)
-                    except exceptions.HttpException:
-                        log.wanr("got an http exception - this will have to be handled later")
-                if what_to_do == "delete of volume":
-                    log.info("- action: %s %s", what_to_do, id)
-                    try:
-                        conn.block_store.delete_volume(id)
-                    except exceptions.HttpException:
-                        log.warn("got an http exception - this will have to be handled later")
-                else:
-                    log.warn("- PLEASE CHECK MANUALLY: unsupported action requested for id - %s", id)
+    def connection_buildup(self):
+        # a dict of all projects we have in openstack
+        self.projects = dict()
+        # openstack connection
+        try:
+            self.conn = connection.Connection(auth_url=os.getenv('OS_AUTH_URL'),
+                                     project_name=os.getenv('OS_PROJECT_NAME'),
+                                     project_domain_name=os.getenv('OS_PROJECT_DOMAIN_NAME'),
+                                     username=os.getenv('OS_USERNAME'),
+                                     user_domain_name=os.getenv('OS_USER_DOMAIN_NAME'),
+                                     password=os.getenv('OS_PASSWORD'))
+        except Exception as e:
+            log.warn("- PLEASE CHECK MANUALLY: problems connecting to openstack: %s - retrying in next loop run",
+                     str(e))
         else:
-            log.info("- plan: %s %s (%i/%i)", what_to_do, id, to_be_dict.get(id, default) + 1, int(iterations))
-        to_be_dict[id] = to_be_dict.get(id, default) + 1
+            # get all openstack projects
+            for project in self.conn.identity.projects(details=False, all_tenants=1):
+                self.projects[project.id] = project.name
 
-# main cleanup function
-def os_cleanup_items(interval, iterations, nova, cinder, dry_run):
-    # openstack connection
-    conn = connection.Connection(auth_url=os.getenv('OS_AUTH_URL'),
-                                 project_name=os.getenv('OS_PROJECT_NAME'),
-                                 project_domain_name=os.getenv('OS_PROJECT_DOMAIN_NAME'),
-                                 username=os.getenv('OS_USERNAME'),
-                                 user_domain_name=os.getenv('OS_USER_DOMAIN_NAME'),
-                                 password=os.getenv('OS_PASSWORD'))
+    def init_seen_dict(self):
+        for i in self.seen_dict:
+            self.seen_dict[i] = 0
 
-    # a dict of all projects we have in openstack
-    projects = dict()
+    # reset dict of all vms or volumes we plan to delete from the db
+    def reset_to_be_dict(self):
+        for i in self.seen_dict:
+            if self.seen_dict[i] == 0:
+                self.to_be_dict[i] = 0
 
-    # get all openstack projects
-    for project in conn.identity.projects(details=False, all_tenants=1):
-        projects[project.id] = project.name
+    def run_me(self):
+        if self.nova or self.cinder:
+            while True:
+                self.connection_buildup()
+                if len(self.projects) > 0:
+                    self.os_cleanup_items()
+                self.wait_a_moment()
+        else:
+            log.info("either the --nova or the --cinder flag should be given - giving up!")
+            sys.exit(0)
 
-    # this should be unified maybe with the cinder stuff below
-    # get all instances from nova
-    if nova:
-        # create a list of servers, sorted by their id
-        all_servers = sorted(conn.compute.servers(details=True, all_tenants=1), key=lambda x: x.id)
-        init_seen_dict(servers_seen)
-        for a_server in all_servers:
-            # instance has an existing project id - we keep it
-            if projects.get(a_server.project_id):
-                log.debug("server %s has a valid project id: %s", str(a_server.id), str(a_server.project_id))
+    # main cleanup function
+    def os_cleanup_items(self):
+
+        # this should be unified maybe with the cinder stuff below
+        # get all instances from nova
+        if self.nova:
+            # create a list of servers, sorted by their id
+            self.entity = sorted(self.conn.compute.servers(details=True, all_tenants=1), key=lambda x: x.id)
+            self.check_for_project_id("server")
+
+        # get all volumes from cinder
+        if self.cinder:
+            # create a list of volumes, sorted by their id
+            self.entity = sorted(self.conn.block_store.volumes(details=True, all_tenants=1), key=lambda x: x.id)
+            self.check_for_project_id("volume")
+
+    def wait_a_moment(self):
+        # wait the interval time
+        time.sleep(60 * int(self.interval))
+
+    def check_for_project_id(self, type):
+        self.init_seen_dict()
+        for element in self.entity:
+            # element has an existing project id - we keep it
+            if self.projects.get(element.project_id):
+                log.debug("%s %s has a valid project id: %s", type, str(element.id), str(element.project_id))
                 pass
-            # instance has no existing project id - we plan to delete it
+            # element has no existing project id - we plan to delete it
             else:
-                log.debug("server %s has no valid project id!", str(a_server.id))
-                now_or_later(a_server.id, servers_to_be_deleted, servers_seen, "delete of server", iterations, dry_run, conn)
+                log.debug("%s %s has no valid project id!", type, str(element.id))
+                self.now_or_later(element.id, "delete of " + type)
         # reset the dict of instances we plan to do delete from the db for all machines we did not see or which disappeared
-        reset_to_be_dict(servers_to_be_deleted, servers_seen)
+        self.reset_to_be_dict()
 
-    # get all volumes from cinder
-    if cinder:
-        # create a list of volumes, sorted by their id
-        all_volumes = sorted(conn.block_store.volumes(details=True, all_tenants=1), key=lambda x: x.id)
-        init_seen_dict(volumes_seen)
-        for a_volume in all_volumes:
-            # volume has an existing project id - we keep it
-            if projects.get(a_volume.project_id):
-                log.debug("volume %s has a valid project id: %s", str(a_volume.id), str(a_volume.project_id))
-                pass
-            # volume has no existing project id - we plan to delete it
+    # here we decide to wait longer before doings the delete from the db or finally doing it
+    def now_or_later(self, id, what_to_do):
+        default = 0
+        self.seen_dict[id] = 1
+        # if we did not see this more often than iteration times, do or dry-run print what to do - otherwise do not print anything, so that dry-run mode looks like real mode
+        if self.to_be_dict.get(id, default) <= int(self.iterations):
+            # we have seen it iteration times, time to act
+            if self.to_be_dict.get(id, default) == int(self.iterations):
+                # ... or print if we are only in dry-run mode
+                if self.dry_run:
+                    log.info("- dry-run: %s %s", what_to_do, id)
+                else:
+                    if what_to_do == "delete of server":
+                        log.info("- action: %s %s", what_to_do, id)
+                        try:
+                            self.conn.compute.delete_server(id)
+                        except exceptions.HttpException:
+                            log.warn("got an http exception - this will have to be handled later")
+                    if what_to_do == "delete of volume":
+                        log.info("- action: %s %s", what_to_do, id)
+                        try:
+                            self.conn.block_store.delete_volume(id)
+                        except exceptions.HttpException:
+                            log.warn("got an http exception - this will have to be handled later")
+                    else:
+                        log.warn("- PLEASE CHECK MANUALLY: unsupported action requested for id - %s", id)
+            # otherwise print out what we plan to do in the future
             else:
-                log.debug("volume %s has no valid project id!", str(a_volume.id))
-                now_or_later(a_volume.id, volumes_to_be_deleted, volumes_seen, "delete of volume", iterations, dry_run, conn)
-        # reset the dict of instances we plan to do delete from the db for all machines we did not see or which disappeared
-        reset_to_be_dict(volumes_to_be_deleted, volumes_seen)
-
-    # wait the interval time
-    time.sleep(60 * int(interval))
+                log.info("- plan: %s %s (%i/%i)", what_to_do, id, self.to_be_dict.get(id, default) + 1, int(self.iterations))
+            self.to_be_dict[id] = self.to_be_dict.get(id, default) + 1
 
 if __name__ == '__main__':
-    while True:
-        run_me()
+    c = Cleanup()
