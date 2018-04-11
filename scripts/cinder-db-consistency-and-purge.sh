230bd6a6dae24cea679bbe512122790c6b834999
Thomas Graichen <thomas.graichen@sap.com>
2018-04-11 14:43:32 +0200

    cleaner output

--- scripts/cinder-db-consistency-and-purge.sh
+++ scripts/cinder-db-consistency-and-purge.sh
@@ -38,7 +38,7 @@ while true; do
         fi
     fi
     if [ "$CINDER_DB_PURGE_ENABLED" = "True" ] || [ "$CINDER_DB_PURGE_ENABLED" = "true" ]; then
-        echo -n "INFO: purging deleted cinder entities older than $CINDER_DB_PURGE_OLDER_THAN days from the cinder db"
+        echo -n "INFO: purging deleted cinder entities older than $CINDER_DB_PURGE_OLDER_THAN days from the cinder db - "
         date
         /var/lib/kolla/venv/bin/cinder-manage db purge $CINDER_DB_PURGE_OLDER_THAN
     fi
