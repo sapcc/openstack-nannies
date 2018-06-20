b2c3c5f8778d1ae1d077a2c2839f216aa85723d8
Maurice Schreiber <maurice.schreiber@sap.com>
2018-06-20 06:51:25 +0200

    add netapp-manila nanny
    for taking over the init_host re-export part on manila-share startup

---
+++ scripts/netapp-manila-reexport.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2018 SAP SE
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# this script re-exports share instances from manila to netapp
+
+from oslo_config import cfg
+from manila import context
+from manila import service
+
+CONF = cfg.CONF
+service_name = 'netapp-multi'
+# host needs to be set in config
+host = "%s@%s" % (CONF.host, service_name)
+
+backend = service.Service.create(binary='manila-share',
+                                    service_name=service_name, host=host)
+ctxt = context.get_admin_context()
+backend.driver.do_setup(ctxt)
+# in contrast to manila-share service enable re-export
+backend.init_host(reexport=True)
---
+++ scripts/netapp-manila-reexport.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 SAP SE
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+set -e
+
+unset http_proxy https_proxy all_proxy no_proxy
+
+echo "INFO: copying manila config files to /etc/manila"
+cp -v /manila-etc/* /etc/manila
+
+# we run an endless loop to run the script periodically
+echo "INFO: starting a loop to periodically run the nanny job for the manila netapp re-export"
+while true; do
+    date
+    /var/lib/kolla/venv/bin/manila-manage --config-file /etc/manila/backend.conf --config-file /etc/manila/manila.conf --config-file /etc/manila/host.conf shell script --path /scripts/netapp-manila-reexport.py
+    echo -n "INFO: waiting $MANILA_NETAPP_NANNY_INTERVAL minutes before starting the next loop run - "
+    date
+    sleep $(( 60 * $MANILA_NETAPP_NANNY_INTERVAL ))
+done
