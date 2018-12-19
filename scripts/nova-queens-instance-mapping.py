e80f6f96310d783e56ba80f0819364f657af7aad
Thomas Graichen <thomas.graichen@sap.com>
2018-12-19 09:48:45 +0100

    add license and use logger for logging of the nova queens instance mapping check

--- scripts/nova-queens-instance-mapping.py
+++ scripts/nova-queens-instance-mapping.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2018 SAP SE
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# this script checks the instance mappings in queens nova
+
 
 import sys
 import psycopg2
 import ConfigParser
 import argparse
+import logging
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
 
 
 def get_api_db_url(config_file):
@@ -12,7 +35,7 @@ def get_api_db_url(config_file):
     parser.read(config_file)
     api_db_url = parser.get('api_database', 'connection', raw=True)
   except:
-    print "ERROR: Check Nova configuration file."
+    log.error("ERROR: Check Nova configuration file.")
     sys.exit(2)
   return api_db_url
 
@@ -37,7 +60,7 @@ def parse_cmdline_args():
 try:
   args = parse_cmdline_args()
 except Exception as e:
-  print("Check command line arguments (%s)", e.strerror)
+  log.error("Check command line arguments (%s)", e.strerror)
 
 # Connect to databases
 api_conn = psycopg2.connect(get_api_db_url(args.config).replace('+psycopg2',''))
@@ -49,7 +72,7 @@ CELLS = [{'id': id, 'name': name, 'db': _get_conn(db)} for id, name, db in api_c
 
 # Get list of all unmapped instances
 api_cur.execute("SELECT instance_uuid FROM instance_mappings WHERE cell_id IS NULL")
-print "Number of unmapped instances: %s" % api_cur.rowcount
+log.info("Number of unmapped instances: %s", api_cur.rowcount)
 
 # Go over all unmapped instances
 unmapped_instances = api_cur.fetchall()
@@ -60,7 +83,7 @@ for (instance_uuid,) in unmapped_instances:
   # Check if a build request exists, if so, skip.
   api_cur.execute("SELECT id FROM build_requests WHERE instance_uuid = %s", (instance_uuid,))
   if api_cur.rowcount != 0:
-    print "%s: build request exists, checking if instance have been scheduled" % (instance_uuid,)
+    log.info("%s: build request exists, checking if instance has been scheduled", (instance_uuid,))
     build_request = True
 
   # Check which cell contains this instance
@@ -73,14 +96,14 @@ for (instance_uuid,) in unmapped_instances:
 
   # Update to the correct cell
   if instance_cell:
-    print "found missing instance mapping to cell %s" % instance_cell['id']
+    log.warn("found missing instance mapping to cell %s", instance_cell['id'])
     if not args.dry_run:
-      print "UPDATE instance_mappings SET cell_id = '%s' WHERE instance_uuid = '%s';" % (instance_cell['id'], instance_uuid)
+      log.info("UPDATE instance_mappings SET cell_id = '%s' WHERE instance_uuid = '%s';", (instance_cell['id'], instance_uuid))
       api_cur.execute("UPDATE instance_mappings SET cell_id = '%s' WHERE instance_uuid = '%s';" % (instance_cell['id'], instance_uuid))
     if build_request:
       if not args.dry_run:
-        print "build requests existing for scheduled instance, deleting build-request to fix instance-list"
+        log.info("build requests existing for scheduled instance, deleting build-request to fix instance-list")
         api_cur.execute("DELETE FROM build_requests WHERE instance_uuid = %s", (instance_uuid,))
 
   # If we reach this point, it's not in any cell?!
-  print "%s: not found in any cell" % (instance_uuid)
+  log.info("%s: not found in any cell", (instance_uuid,))
