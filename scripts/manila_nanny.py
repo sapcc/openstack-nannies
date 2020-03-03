459ede85ffb21a212fc996d19eb21a515f577951
Chuan Miao <chuan137@gmail.com>
2020-03-03 12:44:01 +0100

    [manila] add manila client to ManilaNanny class

--- scripts/manila_nanny.py
+++ scripts/manila_nanny.py
@@ -6,6 +6,9 @@ from sqlalchemy import MetaData
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from manilaclient import client
 
 class ManilaNanny(object):
     def __init__(self, config_file, interval, dry_run=False):
@@ -13,6 +16,7 @@ class ManilaNanny(object):
         self.interval = interval
         self.dry_run = dry_run
         self.initDBConnection()
+        self.manilaclient = create_manila_client(config_file)
 
     def _run(self):
         raise Exception('not implemented')
@@ -43,4 +47,36 @@ class ManilaNanny(object):
             print("ERROR: Parse {}: ".format(config_file) + str(e))
             sys.exit(2)
         return db_url
+
+def create_manila_client(config_file):
+    """  Parse config file and create manila client
+
+        :param string config_file:
+        :return client.Client manila:  manila client
+    """
+    try:
+        parser = ConfigParser.SafeConfigParser()
+        parser.read(config_file)
+        auth_url = parser.get('keystone_authtoken', 'www_authenticate_uri')
+        username = parser.get('keystone_authtoken', 'username')
+        password = parser.get('keystone_authtoken', 'password')
+        user_domain = parser.get('keystone_authtoken', 'user_domain_name')
+        prj_domain = parser.get('keystone_authtoken', 'project_domain_name')
+        prj_name = parser.get('keystone_authtoken', 'project_name')
+    except Exception as e:
+        print "ERROR: Parse {}: ".format(config_file) + e.message
+        sys.exit(2)
+
+    auth = v3.Password(
+        user_domain_name = user_domain,
+        username = username,
+        password = password,
+        project_domain_name= prj_domain,
+        project_name= prj_name,
+        auth_url= auth_url,
+    )
+    sess = session.Session(auth=auth)
+    manila = client.Client("2.0", session=sess)
+    return manila
+
 
