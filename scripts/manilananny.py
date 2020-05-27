fb032a0b3b2f738afbbf22b7c7594bbf562a0d38
chuan137 <chuan137@gmail.com>
2020-05-27 12:55:03 +0200

    add update_dict()

--- scripts/manilananny.py
+++ scripts/manilananny.py
@@ -141,3 +141,13 @@ def response(func):
             message = '\r\n'.join(message_parts)
             return status_code, header, message
     return wrapper_func
+
+def update_dict(target_dict, new_dict):
+    old_dict = target_dict
+    target_dict = {}
+    for key in new_dict:
+        if key in old_dict:
+            target_dict[key] = old_dict[key]
+        else:
+            target_dict[key] = new_dict[key]
+    return target_dict
