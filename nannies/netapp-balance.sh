dc1dbb37131b6cfccb42960042aa8f7a008b697f
Thomas Graichen <thomas.graichen@sap.com>
2020-07-15 11:29:16 +0200

    [netapp] proper dry-run mode

--- nannies/netapp-balance.sh
+++ nannies/netapp-balance.sh
@@ -21,8 +21,7 @@ date
 if [ "$NETAPP_BALANCE_DRY_RUN" = "False" ] || [ "$NETAPP_BALANCE_DRY_RUN" = "false" ]; then
     DRY_RUN=""
 else
-    # DRY_RUN="--dry-run"
-    DRY_RUN=""
+    DRY_RUN="--dry-run"
 fi
 
 python3 /scripts/netapp_balance.py $DRY_RUN --vcenter-host $NETAPP_BALANCE_VCHOST --vcenter-user $NETAPP_BALANCE_VCUSER --vcenter-password $NETAPP_BALANCE_VCPASSWORD --netapp-user $NETAPP_BALANCE_NETAPPUSER --netapp-password $NETAPP_BALANCE_NETAPPPASSWORD --region $NETAPP_BALANCE_REGION --interval $NETAPP_BALANCE_INTERVAL --min-threshold $NETAPP_BALANCE_MINTHRESHOLD --max-threshold $NETAPP_BALANCE_MAXTHRESHOLD --lun-min-size-flexvol $NETAPP_BALANCE_LUNMINSIZEFLEXVOL --lun-max-size-flexvol $NETAPP_BALANCE_LUNMAXSIZEFLEXVOL --lun-min-size-aggr $NETAPP_BALANCE_LUNMINSIZEAGGR --lun-max-size-aggr $NETAPP_BALANCE_LUNMAXSIZEAGGR --max-move-vms $NETAPP_BALANCE_MAXMOVEVMS --max-threshold-hysteresis $NETAPP_BALANCE_MAXTHRESHOLDHYSTERESIS --flexvol-size-limit $NETAPP_BALANCE_FLEXVOLSIZELIMIT
