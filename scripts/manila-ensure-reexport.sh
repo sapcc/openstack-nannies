0add34bd57cc3ac6835fcf9308278fa9aa4be344
Maurice Escher <maurice.escher@sap.com>
2020-08-10 21:07:14 +0200

    manila ensure is no longer named nanny
    though it still is something like a nanny: a reconciliation loop

--- scripts/manila-ensure-reexport.sh
+++ scripts/manila-ensure-reexport.sh
@@ -21,11 +21,11 @@ set -e
 unset http_proxy https_proxy all_proxy no_proxy
 
 # we run an endless loop to run the script periodically
-echo "INFO: starting a loop to periodically run the nanny job for the manila netapp re-export"
+echo "INFO: starting a loop to periodically run the ensure job for the manila netapp re-export"
 while true; do
     date
-    /var/lib/openstack/bin/manila-manage --config-file /etc/manila/backend.conf --config-file /etc/manila/manila.conf shell script --path /scripts/netapp-manila-reexport.py
-    echo -n "INFO: waiting $MANILA_NETAPP_NANNY_INTERVAL minutes before starting the next loop run - "
+    /var/lib/openstack/bin/manila-manage --config-file /etc/manila/backend.conf --config-file /etc/manila/manila.conf shell script --path /scripts/manila-ensure-reexport.py
+    echo -n "INFO: waiting $MANILA_NETAPP_ENSURE_INTERVAL minutes before starting the next loop run - "
     date
-    sleep $(( 60 * $MANILA_NETAPP_NANNY_INTERVAL ))
+    sleep $(( 60 * $MANILA_NETAPP_ENSURE_INTERVAL ))
 done
